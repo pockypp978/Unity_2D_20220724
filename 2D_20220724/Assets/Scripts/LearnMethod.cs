16f0491e6a2387046528f0b8121a2cf36a48b3bd
pockypp978 <109886614+pockypp978@users.noreply.github.com>
2022-08-14 17:06:08 +0800

    Update 03 20220814

---
+++ 2D_20220724/Assets/Scripts/LearnMethod.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace LP
+{
+    public class LearnMethod : MonoBehaviour
+    {
+        public void Test()
+        {
+            print("µ²ªG¡G"+TestTen());
+        }
+        private void Start()
+        {
+            Test();
+        }
+        private int TestTen()
+        {
+            return 10;
+        }
+    }
+}
+
