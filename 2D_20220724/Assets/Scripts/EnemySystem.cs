48ef28986e39b802d2b27800f635b0fe03519c49
pockypp978 <109886614+pockypp978@users.noreply.github.com>
2022-09-11 17:00:45 +0800

    Update 06 20220911

--- 2D_20220724/Assets/Scripts/EnemySystem.cs
+++ 2D_20220724/Assets/Scripts/EnemySystem.cs
@@ -7,6 +7,7 @@ public class EnemySystem : MonoBehaviour
     private EnemyData enemyData;
 
     private Rigidbody2D rig;
+    private bool isGroundForward;
 
 
 
@@ -25,6 +26,7 @@ public class EnemySystem : MonoBehaviour
     {
         Wander();
         CheckGroundForward();
+        Flip();
     }
     /// <summary>
     /// 遊走功能
@@ -41,6 +43,16 @@ public class EnemySystem : MonoBehaviour
         Collider2D hit = Physics2D.OverlapBox(
             transform.position + enemyData.checkGroundOffset,
             enemyData.checkGroundSize, 0, enemyData.checkGroundLayer);
-        print("前方碰撞物件" + hit.gameObject);
+        //print("前方碰撞物件" + hit.gameObject);
+        isGroundForward = hit;
+    }
+
+    private void Flip()
+    {
+        if (!isGroundForward)
+        {
+            float yAngle = transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, yAngle == 0 ? 180 : 0,0);
+        }
     }
 }
